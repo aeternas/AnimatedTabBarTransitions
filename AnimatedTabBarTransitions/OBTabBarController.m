b32a0d55e8c4940fd0707939186777f42bc340b9
Ivan Golikov <ivgolikov7@gmail.com>
2015-12-29 10:30:05 +0300

    Added ease-in effect

--- AnimatedTabBarTransitions/OBTabBarController.m
+++ AnimatedTabBarTransitions/OBTabBarController.m
@@ -97,15 +97,13 @@ NSString *const OBTabBarControllerErrorDomain = @"OBTabBarControllerErrorDomain"
     // getting gap between initial and target tabbaritem
     NSInteger delta = [self.tabBar.items indexOfObject:item] - self.viewPosition;
     CGFloat totalDuration = 1.0;
-    [UIView animateKeyframesWithDuration:totalDuration delay:0.0 options:UIViewKeyframeAnimationOptionCalculationModeLinear animations:^{
+    [UIView animateKeyframesWithDuration:totalDuration delay:0.0 options:UIViewKeyframeAnimationOptionCalculationModeLinear | UIViewAnimationOptionCurveEaseIn animations:^{
         // getting absolute value in order to code right-to-left animations
         NSInteger modulusDelta = labs(delta);
         __block CGFloat relativeStartTimeForAppearingView = 0.0;
         __block CGFloat relativeStartTimeForDisappearingView = 0.5;
         for (int i = 0; i < modulusDelta; i++) {
             CGFloat relativeDuration = (totalDuration / modulusDelta);
-            
-            
             
             // using "child" keyframe animations
             [UIView addKeyframeWithRelativeStartTime:relativeStartTimeForDisappearingView relativeDuration:relativeDuration animations:^{
